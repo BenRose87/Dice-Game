c80a77e3b7fdc3c8ada0d910fd79900aab3c24ff
BenRose87 <68743675+BenRose87@users.noreply.github.com>
2020-08-27 16:37:53 +0200

    Now showing refreshed me first

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
-document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+if (performance.navigation.type === 1) {
 
-if (randomNumber1 === randomNumber2) {
-  document.querySelector("h1").innerHTML = "Draw!";
-}
+  document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+  document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
-else if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
-}
+  if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
 
-else {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  else if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  }
+
+  else {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  }
 }
